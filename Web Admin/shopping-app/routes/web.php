b42faae5aa89b491153e0e250d0383d04da64dab
iamhuonggiang08 <gianghuongtruong2708@gmail.com>
2024-11-03 22:42:51 +0700

    update

--- Web Admin/shopping-app/routes/web.php
+++ Web Admin/shopping-app/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin/menu', function () {
+    return view('menu');
+});
+Route::get('/admin/orders', function () {
+    return view('order');
+});
